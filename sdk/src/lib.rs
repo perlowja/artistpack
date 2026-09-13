d9b5af5fe0be4894d47d628996069d81f8509aa7
Jason Perlow <jperlow@gmail.com>
2026-09-13 16:08:36 -0400

    feat(sdk): workspace skeleton with FORMAT_VERSION constant

---
+++ sdk/src/lib.rs
@@ -0,0 +1,11 @@
+pub const FORMAT_VERSION: &str = "0.1";
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn format_version_matches_spec() {
+        assert_eq!(FORMAT_VERSION, "0.1");
+    }
+}
